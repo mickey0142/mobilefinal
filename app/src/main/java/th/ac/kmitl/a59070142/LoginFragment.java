53dd94351fa855175ea7819713a499506da2e30a
LAB203_05 <59070142@it.kmitl.ac.th>
2018-12-11 09:57:38 +0700

    initial commit

---
+++ app/src/main/java/th/ac/kmitl/a59070142/LoginFragment.java
@@ -0,0 +1,25 @@
+package th.ac.kmitl.a59070142;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+}
---
+++ app/src/main/java/th/ac/kmitl/a59070142/MainActivity.java
@@ -0,0 +1,20 @@
+package th.ac.kmitl.a59070142;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null)
+        {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_view, new LoginFragment())
+                    .commit();
+        }
+    }
+}
